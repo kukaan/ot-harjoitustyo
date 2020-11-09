57311fcc930822d728cc7cf80b5c625d98794b1c
kukaan <58886775+kukaan@users.noreply.github.com>
2020-11-09 17:35:11 +0200

    t2

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -10,11 +10,43 @@ public class MaksukorttiTest {
 
     @Before
     public void setUp() {
-        kortti = new Maksukortti(10);
+        kortti = new Maksukortti(1000);
     }
 
     @Test
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
+    }
+    
+    @Test
+    public void saldoTulostuuAlussaOikein() {
+        assertEquals("saldo: 10.0",kortti.toString());
+    }
+    
+    @Test
+    public void saldoSentitAlussaOikein() {
+        assertEquals(1000, kortti.saldo());
+    }
+    
+    @Test
+    public void saldoVaheneeOikein() {
+        kortti.otaRahaa(1000);
+        assertEquals(0, kortti.saldo());
+    }
+    
+    @Test
+    public void saldoEiMuutuJosEiTarpeeksiRahaa() {
+        kortti.otaRahaa(1001);
+        assertEquals(1000, kortti.saldo());
+    }
+    
+    @Test
+    public void ottoPalauttaaTrueOikein() {
+        assertEquals(true, kortti.otaRahaa(1000));
+    }
+    
+    @Test
+    public void ottoPalauttaaFalseOikein() {
+        assertEquals(false, kortti.otaRahaa(1001));
     }
 }
